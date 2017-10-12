2ae0b3b94487461b9ffdeb2f9e2e4dfecb757a36
Grzegorz <grzegorz2047@gmail.com>
2017-10-12 09:56:08 +0200

    Add files via upload

---
+++ ProductManagement/src/main/java/pl/grzegorz2047/productmanagement/ProductManagementApplication.java
@@ -0,0 +1,12 @@
+package pl.grzegorz2047.productmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApplication.class, args);
+	}
+}

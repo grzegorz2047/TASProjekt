4f91d578471248767465d0242fdeba61ffd7e8e3
grzegorz2047 <grzegorz2047@gmail.com>
2017-10-19 11:13:53 +0200

    Dodano autoryzację oraz domyślnego użytkownika.

--- ProductManagement/src/main/java/pl/grzegorz2047/productmanagement/ProductManagementApplication.java
+++ ProductManagement/src/main/java/pl/grzegorz2047/productmanagement/ProductManagementApplication.java
@@ -3,9 +3,11 @@ package pl.grzegorz2047.productmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
+@ComponentScan(basePackages = "pl.grzegorz2047")
 public class ProductManagementApplication {
 
     public static void main(String[] args) {
---
+++ ProductManagement/src/main/java/pl/grzegorz2047/productmanagement/security/SpringSecurityConfig.java
@@ -0,0 +1,32 @@
+package pl.grzegorz2047.productmanagement.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationEntryPoint authEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("grzegorz").password("123").roles("USER");
+    }
+
+}
